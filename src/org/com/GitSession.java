a15541aace5c020aaec3d63f23e1406bba0a0c97
prakashvalarthasan <grittierprakash1@gmail.com>
2021-07-23 08:19:12 +0530

    Linda work is done

--- src/org/com/GitSession.java
+++ src/org/com/GitSession.java
@@ -1,8 +1,13 @@
 package org.com;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class GitSession {
 	public static void main(String[] args) {
 		System.out.println("Day 1...");
+		List<String> s = new LinkedList<>();
+		s.add("Linda");
 	}
 
 }
